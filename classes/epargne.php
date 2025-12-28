d4fa5e9b1961c65aface78aaac84669e46755b2d
HIBA53757 <hibatallahsadiki20@gmail.com>
2025-12-28 18:24:49 +0100

    BS-22 creation de database

---
+++ classes/epargne.php
@@ -0,0 +1,22 @@
+<?php
+require_once "compte.php";
+
+class Compte_Epargne extends Compte
+{
+    public function deposer($montant)
+    {
+        $this->solde += $montant;
+        $this->update();
+    }
+
+    public function retirer($montant)
+    {
+        if ($this->solde < $montant) {
+            throw new Exception("Retrait refusé : solde insuffisant.");
+        }
+        $this->solde -= $montant;
+        $this->update();
+    }
+}
+?>
+
